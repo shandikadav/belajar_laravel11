432a0dee5157e312241ea496f9334e015b60c5fb
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-06-04 20:17:16 +0700

    menyelesaikan materi view data

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,14 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
             'id' => '1',
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Shandika David Ardiansyah',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
         ],
         [
             'id' => '2',
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Shandika David Ardiansyah',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
@@ -32,24 +34,26 @@ Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
 
-Route::get('/posts/{id}', function ($id) {
+Route::get('/posts/{slug}', function ($slug) {
     $posts = [
         [
             'id' => '1',
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Shandika David Ardiansyah',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
         ],
         [
             'id' => '2',
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Shandika David Ardiansyah',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
         ]
     ];
 
-    $post = Arr::first($posts, function($post) use($id) {
-        return $post['id'] == $id;
+    $post = Arr::first($posts, function($post) use($slug) {
+        return $post['slug'] == $slug;
     });
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
